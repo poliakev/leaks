8992c2babd6346337bfba26cef1c779ededf3f71
Jan Poliakevitch <jpoliakevitch@gmail.com>
2015-02-01 16:32:23 -0700

    globs

---
+++ Assets/MopScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MopScript : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "drop")
+		{
+			Destroy(other.gameObject);
+		}
+	}
+}
