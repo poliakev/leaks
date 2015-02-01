6e81cc532c2da71dec283ef230eefc8bdcbc5cf7
Jan Poliakevitch <jpoliakevitch@gmail.com>
2015-02-01 15:07:14 -0700

    Progress

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float JetPackForce = 10;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			rigidbody2D.AddForce (Vector2.up * JetPackForce);
+			
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			rigidbody2D.AddForce (-Vector2.right * JetPackForce);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			rigidbody2D.AddForce (Vector2.right * JetPackForce);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			rigidbody2D.AddForce (-Vector2.up * JetPackForce);
+		}
+	}
+}
